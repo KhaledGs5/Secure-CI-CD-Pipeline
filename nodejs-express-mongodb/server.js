011bc442b1ac0ee96a7903db8b0fa8cdffd57c3b
Khaled Gassara <khaled.gassara@ensi-uma.tn>
2024-10-31 09:27:26 +0100

    configuring virtual environment

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-
+var mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
-var corsOptions = {
-  origin: "http://localhost:4200"
-};
+// var corsOptions = {
+//   origin: "http://localhost:4200"
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
@@ -19,21 +20,13 @@ app.get("/", (req, res) => {
 
 require("./app/routes/teams.routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// const db = require("./app/models");
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err.message));
